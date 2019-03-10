35be6e476e2efbf1dcf4b83392595ca6321ac193
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-10 14:25:27 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -19,6 +19,7 @@ def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
 
+
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
@@ -36,3 +37,7 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+
+end
